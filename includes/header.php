5d89906c46875979592e5c38d9c43dc3e08edaee
Anjara Tantely <58203564+tantely-bee@users.noreply.github.com>
2023-10-18 13:38:31 +0300

    .

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>ENI</title>
+    <title>PHP MySQL CRUD</title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <!-- BOOTSTRAP 4 -->
     <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
@@ -13,7 +13,7 @@
 
     <nav class="navbar navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="index.php">PHP MySQL CRUD</a>
+        <a class="navbar-brand" href="index.php">ENI</a>
       </div>
     </nav>
 
