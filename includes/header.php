eb83561915d18e9024974a992c7ea61a18952594
Anjara Tantely <58203564+tantely-bee@users.noreply.github.com>
2023-11-09 22:02:38 +0300

    K

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
 
     <nav class="navbar navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="index.php"><h1>ALLO</h1></a>
+        <a class="navbar-brand" href="index.php"><h1>KJKK</h1></a>
       </div>
     </nav>
 
