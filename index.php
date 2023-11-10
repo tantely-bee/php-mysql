6573c74bdf5a286a4691dc8445c1733bfddb8e54
Anjara Tantely <58203564+tantely-bee@users.noreply.github.com>
2023-11-10 18:15:26 +0300

    FF

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
         <thead>
           <tr>
             <th>Tâches</th>
-            <th>test123</th>
+            <th>Description</th>
             <th>Crée le</th>
             <th>Action</th>
           </tr>
