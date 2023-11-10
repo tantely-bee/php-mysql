ae24d6777544665ab66cf67877a1467d4fa2b999
Anjara Tantely <58203564+tantely-bee@users.noreply.github.com>
2023-11-10 11:15:33 +0300

    dd

--- index.php
+++ index.php
@@ -20,12 +20,12 @@
       <div class="card card-body">
         <form action="save_task.php" method="POST">
           <div class="form-group">
-            <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
+            <input type="text" name="title" class="form-control" placeholder="Titre du tâche" autofocus>
           </div>
           <div class="form-group">
-            <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
+            <textarea name="description" rows="2" class="form-control" placeholder="Description du tâche"></textarea>
           </div>
-          <input type="submit" name="save_task" class="btn btn-success btn-block" value="Save Task">
+          <input type="submit" name="save_task" class="btn btn-success btn-block" value="Enregistrer tâche">
         </form>
       </div>
     </div>
